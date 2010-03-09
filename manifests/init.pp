db4ffc2856a26182f76da78e47911facaa593e59
David Schmitt <david@dasz.at>
2010-03-09 09:56:23 +0100

    update to modules/ fileserver mount of 0.25

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,19 +5,23 @@
 import "defines/*.pp"
 import "classes/*.pp"
 
-module_dir { [ 'common' ]: }
+class common {
+	module_dir { [ 'common' ]: }
 
-file {
-		 # Module programmers can use /var/lib/puppet/modules/$modulename to save
-		 # module-local data, e.g. for constructing config files. See module_dir
-		 # for details
-       "/var/lib/puppet/modules":
-               ensure => directory,
-               source => "puppet:///common/modules",
-               ignore => ".ignore",
-               recurse => true, purge => true, force => true,
-               mode => 0755, owner => root, group => 0;
+	file {
+		# Module programmers can use /var/lib/puppet/modules/$modulename to save
+		# module-local data, e.g. for constructing config files. See module_dir
+		# for details
+		"/var/lib/puppet/modules":
+			ensure => directory,
+			source => "puppet:///modules/common/modules",
+			ignore => ".ignore",
+			recurse => true, purge => true, force => true,
+			mode => 0755, owner => root, group => 0;
+	}
 }
+
+include common
 
 # common packages
 class pkg::openssl { package { openssl: ensure => installed } }
