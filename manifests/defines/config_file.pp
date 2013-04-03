4a992afc4b8779710974808cd6cf810b3fd23401
Andre timmermann <andre.timmermann@darkt.im>
2013-04-03 14:37:35 +0200

    puppet lint  --no-autoloader_layout-check --no-80chars-check

--- manifests/defines/config_file.pp
+++ manifests/defines/config_file.pp
@@ -3,58 +3,60 @@
 # See LICENSE for the full license granted to you.
 
 # A simple wrapper to give all configuration files common defaults.
-# 
+#
 # Usage:
 #  config_file { filename:
-#  	content => "....\n",
+#    content => "....\n",
 #  }
 #
-# Examples: 
+# Examples:
 #
 # To create the file /etc/vservers/${vs_name}/context with specific
 # content:
 #
 #  config_file {
-#  	"/etc/vservers/${vs_name}/context":
-#  		content => "${context}\n",
-#  		notify => Exec["vs_restart_${vs_name}"],
-#  		require => Exec["vs_create_${vs_name}"];
+#    "/etc/vservers/${vs_name}/context":
+#      content => "${context}\n",
+#      notify => Exec["vs_restart_${vs_name}"],
+#      require => Exec["vs_create_${vs_name}"];
 # }
 #
 # To create the file /etc/apache2/sites-available/munin-stats with the
 # content pulled from a template:
 #
 #  config_file {
-# 		"/etc/apache2/sites-available/munin-stats":
-#  		content => template("apache/munin-stats"),
-#  		require => Package["apache2"],
-#  		notify => Exec["reload-apache2"];
+#     "/etc/apache2/sites-available/munin-stats":
+#      content => template("apache/munin-stats"),
+#      require => Package["apache2"],
+#      notify => Exec["reload-apache2"];
 #  }
 define config_file (
-	$content = '',
-	$source = '',
-	$ensure = 'present')
-{
-	file { $name:
-		ensure => $ensure,
-		# keep old versions on the server
-		backup => server,
-		# default permissions for config files
-		mode => 0644, owner => root, group => 0,
-		# really detect changes to this file
-		checksum => md5,
-	}
+    $content = '',
+    $source = '',
+    $ensure = 'present')
+    {
+        file { $name:
+            ensure   => $ensure,
+            # keep old versions on the server
+            backup   => server,
+            # default permissions for config files
+            mode     => '0644',
+            owner    => root,
+            group    => 0,
+            # really detect changes to this file
+            checksum => md5,
+        }
 
-	case $source {
-		'': { }
-		default: { File[$name] { source => $source } }
-	}
+        case $source {
+            '': { }
+            default: { File[$name] { source => $source } }
+        }
 
-	case $content {
-		'': { }
-		default: { File[$name] { content => $content } }
-	}
-				
-}
+        case $content {
+            '': { }
+            default: { File[$name] { content => $content } }
+        }
+
+    }
 
 
--- manifests/defines/replace.pp
+++ manifests/defines/replace.pp
@@ -10,13 +10,13 @@
 # This define uses perl regular expressions.
 #
 # Use this only for very trivial stuff. Usually replacing the whole file is a
-# more stable solution with less maintenance headaches afterwards. 
-# 
+# more stable solution with less maintenance headaches afterwards.
+#
 # Usage:
 #
-# replace { description: 
-#           file => "filename",
-#           pattern => "regexp",
+# replace { description:
+#           file        => "filename",
+#           pattern     => "regexp",
 #           replacement => "replacement"
 #
 # Example:
@@ -24,18 +24,18 @@
 # with a new port, but only disturbing the file when needed:
 #
 #  replace {
-#  	set_munin_node_port:
-#  		file => "/etc/munin/munin-node.conf",
-#  		pattern => "^port (?!$port)[0-9]*",
-#  		replacement => "port $port"
-#  }  
+#    set_munin_node_port:
+#      file        => "/etc/munin/munin-node.conf",
+#      pattern     => "^port (?!$port)[0-9]*",
+#      replacement => "port $port"
+#  }
 define replace($file, $pattern, $replacement) {
-	$pattern_no_slashes = regsubst($pattern, '/', '\\/', 'G', 'U')
-	$replacement_no_slashes = regsubst($replacement, '/', '\\/', 'G', 'U')
+    $pattern_no_slashes = regsubst($pattern, '/', '\\/', 'G', 'U')
+    $replacement_no_slashes = regsubst($replacement, '/', '\\/', 'G', 'U')
 
-	exec { "replace_${pattern}_${file}":
-		command => "/usr/bin/perl -pi -e 's/${pattern_no_slashes}/${replacement_no_slashes}/' '${file}'",
-		onlyif => "/usr/bin/perl -ne 'BEGIN { \$ret = 1; } \$ret = 0 if /${pattern_no_slashes}/ && ! /\\Q${replacement_no_slashes}\\E/; END { exit \$ret; }' '${file}'",
-		alias => "exec_$name",
-	}
+    exec { "replace_${pattern}_${file}":
+        command => "/usr/bin/perl -pi -e 's/${pattern_no_slashes}/${replacement_no_slashes}/' '${file}'",
+        onlyif  => "/usr/bin/perl -ne 'BEGIN { \$ret = 1; } \$ret = 0 if /${pattern_no_slashes}/ && ! /\\Q${replacement_no_slashes}\\E/; END { exit \$ret; }' '${file}'",
+        alias   => "exec_${name}",
+    }
 }
